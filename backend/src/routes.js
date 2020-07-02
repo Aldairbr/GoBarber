ae6c9634fb0f067d8750a7854863d8aed747e934
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-07-02 14:38:50 -0300

    fix/ route 'update and route midleware added

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,23 @@
 import { Router } from 'express'
 
 import userController from './app/controllers/userController'
-
 import sessionController from './app/controllers/sessionController'
+
+import authMidleware from './app/midlewares/auth'
 
 const routes = new Router()
 
 routes.post('/users', userController.store)
 routes.get('/users', userController.index)
 routes.get('/users/:id', userController.show)
-
 routes.post('/sessions', sessionController.store)
+
+routes.use(authMidleware)//midleware vai pegar de maneira global todas as rotas
+                        //abaixo dessa linha. Poderia por direto na rota
+                        //ex:
+                        //routes.put(
+                        // '/users', authMidleware userController.update)
+
+routes.put('/users', userController.update)
 
 export default routes
