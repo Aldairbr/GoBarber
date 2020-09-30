d295ec337d259b4eee835dd892809c831970624f
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-09-30 19:22:24 -0300

    added validator to usaer

--- backend/src/app/controllers/userController.js
+++ backend/src/app/controllers/userController.js
@@ -1,10 +1,26 @@
+import * as Yup from 'yup'
 import User from '../models/User'
 
 class userController {
 
- async store(request, response) {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6)
+    })
 
-    const userExists = await User.findOne({ where: { email: request.body.email } })
+  // esse isValid abaixo é async, por isso devemos usar o await no condicional
+
+    if(!(await schema.isValid(request.body))){
+      return response.status(401).json({ERROR: 'Validation errors'})
+    }
+
+  const userExists = await User.findOne({ where: { email: request.body.email } })
 
     if(userExists){
       return response.status(400).json({ERROR: 'Email already exists!' })
@@ -21,21 +37,39 @@ class userController {
 
   async update(request, response) {
 
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+        oldPassword ? field.required() : field
+        ),
+      // confirmPassword: Yup.string().when('password', (password, field) =>
+      //   password ? field.required().oneOf([Yup.ref('password')]) : field
+      // ),
+    })
+
+      if(!(await schema.isValid(request.body))){
+        return response.status(401).json({ERROR: 'Validation fails'})
+      }
+
     const { email, oldPassword } = request.body
 
     const user = await User.findByPk(request.userId)
 
-    if(email !== user.email){
-      const userExists = await User.findOne({ where: { email } })
+      if(email !== user.email){
+        const userExists = await User.findOne({ where: { email } })
 
       if(!userExists){
-        return response.status(400).json({ERROR: 'User already exists!' })
+          return response.status(400).json({ERROR: 'User already exists!' })
+        }
       }
-    }
 
-    if(oldPassword && !( await checkPassword(oldPassword))){
-      return response.status(401).json({ERROR: 'Password does not match!'})
-    }
+      if(oldPassword && !( await user.checkPassword(oldPassword))){
+        return response.status(401).json({ERROR: 'Password does not match!'})
+      }
 
     const { id, name, provider } = user.update(request.body)
 
