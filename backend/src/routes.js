bcc86fabfc705f511d13e093f966975e9aa641be
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-09-30 19:18:55 -0300

    added new route and imported multer

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
 
 import userController from './app/controllers/userController'
 import sessionController from './app/controllers/sessionController'
@@ -6,6 +8,7 @@ import sessionController from './app/controllers/sessionController'
 import authMidleware from './app/midlewares/auth'
 
 const routes = new Router()
+const upload = multer(multerConfig)
 
 routes.post('/users', userController.store)
 routes.get('/users', userController.index)
@@ -19,5 +22,9 @@ routes.use(authMidleware)//midleware vai pegar de maneira global todas as rotas
                         // '/users', authMidleware userController.update)
 
 routes.put('/users', userController.update)
+
+routes.post('/files', upload.single('file'), (request, response) => {
+  return response.json({message: true})
+})
 
 export default routes
