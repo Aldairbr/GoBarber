54b78a376407d028552789ba3e079023fba7a188
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-10-02 00:49:02 -0300

    added providers route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,8 @@ import multerConfig from './config/multer'
 
 import userController from './app/controllers/userController'
 import sessionController from './app/controllers/sessionController'
+import fileController from './app/controllers/fileController'
+import providerController from './app/controllers/providerController'
 
 import authMidleware from './app/midlewares/auth'
 
@@ -23,8 +25,8 @@ routes.use(authMidleware)//midleware vai pegar de maneira global todas as rotas
 
 routes.put('/users', userController.update)
 
-routes.post('/files', upload.single('file'), (request, response) => {
-  return response.json({message: true})
-})
+routes.get('/providers', providerController.index)
+
+routes.post('/files', upload.single('file'), fileController.store)
 
 export default routes
