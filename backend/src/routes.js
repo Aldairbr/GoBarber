18f6b4f41210046bf0d3c21512c6c91374b74a85
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-10-06 23:50:35 -0300

    added new routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,32 +1,37 @@
-import { Router } from 'express'
-import multer from 'multer'
-import multerConfig from './config/multer'
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-import userController from './app/controllers/userController'
-import sessionController from './app/controllers/sessionController'
-import fileController from './app/controllers/fileController'
-import providerController from './app/controllers/providerController'
+import userController from './app/controllers/userController';
+import sessionController from './app/controllers/sessionController';
+import fileController from './app/controllers/fileController';
+import providerController from './app/controllers/providerController';
+import AppointmentController from './app/controllers/appointmentController';
 
-import authMidleware from './app/midlewares/auth'
+import authMidleware from './app/midlewares/auth';
 
-const routes = new Router()
-const upload = multer(multerConfig)
+const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.post('/users', userController.store)
-routes.get('/users', userController.index)
-routes.get('/users/:id', userController.show)
-routes.post('/sessions', sessionController.store)
+routes.post('/users', userController.store);
+routes.get('/users', userController.index);
+routes.get('/users/:id', userController.show);
+routes.post('/sessions', sessionController.store);
 
-routes.use(authMidleware)//midleware vai pegar de maneira global todas as rotas
-                        //abaixo dessa linha. Poderia por direto na rota
-                        //ex:
-                        //routes.put(
-                        // '/users', authMidleware userController.update)
+routes.use(authMidleware); // midleware vai pegar de maneira global todas as rotas
+// abaixo dessa linha. Poderia por direto na rota
+// ex:
+// routes.put(
+// '/users', authMidleware userController.update)
 
-routes.put('/users', userController.update)
+routes.put('/users', userController.update);
 
-routes.get('/providers', providerController.index)
+routes.get('/providers', providerController.index);
 
-routes.post('/files', upload.single('file'), fileController.store)
+routes.post('/appointments', AppointmentController.store);
 
-export default routes
+routes.get('/appointments', AppointmentController.index);
+
+routes.post('/files', upload.single('file'), fileController.store);
+
+export default routes;
