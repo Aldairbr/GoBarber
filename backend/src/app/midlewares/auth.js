6288a9957448252756316edff00a85007695ab34
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-10-13 00:43:00 -0300

    feat: created method to mark notifications as read

--- backend/src/app/midlewares/auth.js
+++ backend/src/app/midlewares/auth.js
@@ -1,28 +1,27 @@
-import jwt from 'jsonwebtoken'
-import { promisify } from 'util'
+/* eslint-disable no-unreachable */
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
 
-import authConfig from '../../config/auth'
+import authConfig from '../../config/auth';
 
 export default async (request, response, next) => {
+  const authHeader = request.headers.authorization;
 
-  const authHeader = request.headers.authorization
+  if (!authHeader) {
+    return response.status(401).json({ Error: 'Token not provided !' });
+  }
 
-    if(!authHeader) {
-      return response.status(401).json({Error: 'Token not provided !'})
-    }
+  const [, token] = authHeader.split(' ');
 
-    const [, token] = authHeader.split(' ')
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    try {
-      const decoded = await promisify(jwt.verify)(token, authConfig.secret)
+    request.userId = decoded.id;
 
-      request.userId = decoded.id
+    return next();
+  } catch (err) {
+    return response.status(401).json({ Error: 'Invalid token' });
+  }
 
-      return next()
-    } catch (err) {
-
-      return response.status(401).json({ Error: 'Invalid token'})
-    }
-
-  return next()
-}
+  return next();
+};
